a07fde7aef110e46304cd8fe1dd4613e2749660b
Simon Wiles <simonjwiles@gmail.com>
2021-01-24 14:40:25 -0800

    Allow passing ENV_FILE as an env var
    Defaults to .env in the project directory, but it's useful to have multiple variants, for testing at least.

--- sati-backup.sh
+++ sati-backup.sh
@@ -16,20 +16,28 @@ BACKUP_DIR=${BACKUP_DIR:=$(absdir "${BASH_SOURCE%/*}")};
 MEDIA_DIR=${MEDIA_DIR:=$(absdir "${BACKUP_DIR}/../sati/media")};
 COMPOSE_PROJ_DIR=${COMPOSE_PROJ_DIR:=$(absdir "${BACKUP_DIR}/../sati/")};
 COMPOSE_FILE=$(readlink -f "${COMPOSE_FILE:="${COMPOSE_PROJ_DIR}/docker-compose.yml"}");
+ENV_FILE=$(readlink -f "${ENV_FILE:="${COMPOSE_PROJ_DIR}/.env"}");
 
 [ -d "${BACKUP_DIR}" ] || { echo "Backup dir ${BACKUP_DIR} does not exist!" >&2; exit 1; }
 [ -d "${MEDIA_DIR}" ] || { echo "Media dir ${MEDIA_DIR} does not exist!" >&2; exit 1; }
 [ -d "${COMPOSE_PROJ_DIR}" ] || { echo "docker-compose dir ${COMPOSE_PROJ_DIR} does not exist!" >&2; exit 1; }
 [ -e "${COMPOSE_FILE}" ] || { echo "docker-compose file not found at ${COMPOSE_FILE}!" >&2; exit 1; }
+[ -e "${ENV_FILE}" ] || { echo ".env file not found at ${ENV_FILE}!" >&2; exit 1; }
 
 # dump data from the Users app
-docker-compose --project-directory "${COMPOSE_PROJ_DIR}" --file "${COMPOSE_FILE}" \
+docker-compose \
+  --env-file "${ENV_FILE}" \
+  --project-directory "${COMPOSE_PROJ_DIR}" \
+  --file "${COMPOSE_FILE}" \
   exec django python manage.py dumpdata users \
   | jq "[.[] | {model, pk, fields: .fields | del(.date_joined, .last_login, .require_password_change)}]" \
   > "${BACKUP_DIR}/users.json";
 
 # dump data from the Items app
-docker-compose --project-directory "${COMPOSE_PROJ_DIR}" --file "${COMPOSE_FILE}" \
+docker-compose \
+  --env-file "${ENV_FILE}" \
+  --project-directory "${COMPOSE_PROJ_DIR}" \
+  --file "${COMPOSE_FILE}" \
   exec django python manage.py dumpdata --indent 2 items \
   > "${BACKUP_DIR}/items.json";
 
