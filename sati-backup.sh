c993b9571c0f069cc8966f073de1f4422da653b5
Simon Wiles <simonjwiles@gmail.com>
2021-01-25 13:31:20 -0800

    Make sati-backup.sh a better *nix citizen

--- sati-backup.sh
+++ sati-backup.sh
@@ -2,7 +2,71 @@
 
 set -euo pipefail;
 
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-q]
+
+  Backs up user data from the SATI application -- all the database data from the
+  Items and Users apps, as well as the uploaded item images -- and commits new or
+  modified data to GitHub.
+
+    -h          display this help and exit
+    -q          quiet operation; will only produce output in case of error
+                (intended for use from cron)
+
+
+Respects the following environment variables (the defaults are expected to be
+used in production).
+
+  BACKUP_DIR          Directory that contains the backup repo
+                       - defaults to this script's parent dir
+  COMPOSE_PROJ_DIR    Context directory for docker-compose
+                       - defaults to \$BACKUP_DIR/../sati
+  COMPOSE_FILE        docker-compose configuration file
+                       - defaults to \$COMPOSE_PROJ_DIR/docker-compose.yml
+  MEDIA_DIR           Directory that contains the item images (Django's MEDIA_ROOT)
+                       - defaults to \$COMPOSE_PROJ_DIR/media
+  ENV_FILE            .env file passed both to docker-compose and to the containers
+                       - defaults to \$COMPOSE_PROJ_DIR/.env
+
+EOF
+}
+
+quiet=;
+
+while :; do
+    case ${1-} in
+        -h|-\?|--help)
+            show_help
+            exit
+            ;;
+        -q|--quiet)
+            quiet=--quiet
+            ;;
+        --)
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)
+            break
+    esac
+
+    shift
+done
+
 function absdir { cd "$1" && pwd -P; }
+
+function log() {
+  [ $quiet ] && return;
+  [ ! -t 1 ] && printf "\n%s\n" "$1" && return;
+  COLOR='\e[;93m'; # light yellow (93)
+  RESET='\e[0m';
+  printf "\n%b%s%b\n" "${COLOR}" "$1" "${RESET}";
+}
+
 
 # Note: supplying --project-directory "${COMPOSE_PROJ_DIR}" to
 #       docker-compose exec is necessary to have values from the .env
@@ -13,9 +77,9 @@ function absdir { cd "$1" && pwd -P; }
 
 
 BACKUP_DIR=${BACKUP_DIR:=$(absdir "${BASH_SOURCE%/*}")};
-MEDIA_DIR=${MEDIA_DIR:=$(absdir "${BACKUP_DIR}/../sati/media")};
 COMPOSE_PROJ_DIR=${COMPOSE_PROJ_DIR:=$(absdir "${BACKUP_DIR}/../sati/")};
 COMPOSE_FILE=$(readlink -f "${COMPOSE_FILE:="${COMPOSE_PROJ_DIR}/docker-compose.yml"}");
+MEDIA_DIR=${MEDIA_DIR:=$(absdir "${COMPOSE_PROJ_DIR}/media")};
 ENV_FILE=$(readlink -f "${ENV_FILE:="${COMPOSE_PROJ_DIR}/.env"}");
 
 [ -d "${BACKUP_DIR}" ] || { echo "Backup dir ${BACKUP_DIR} does not exist!" >&2; exit 1; }
@@ -24,7 +88,7 @@ ENV_FILE=$(readlink -f "${ENV_FILE:="${COMPOSE_PROJ_DIR}/.env"}");
 [ -e "${COMPOSE_FILE}" ] || { echo "docker-compose file not found at ${COMPOSE_FILE}!" >&2; exit 1; }
 [ -e "${ENV_FILE}" ] || { echo ".env file not found at ${ENV_FILE}!" >&2; exit 1; }
 
-# dump data from the Users app
+log "Dumping data from the Users app to ${BACKUP_DIR}/users.json";
 docker-compose \
   --env-file "${ENV_FILE}" \
   --project-directory "${COMPOSE_PROJ_DIR}" \
@@ -33,7 +97,7 @@ docker-compose \
   | jq "[.[] | {model, pk, fields: .fields | del(.date_joined, .last_login, .require_password_change)}]" \
   > "${BACKUP_DIR}/users.json";
 
-# dump data from the Items app
+log "Dumping data from the Items app to ${BACKUP_DIR}/items.json";
 docker-compose \
   --env-file "${ENV_FILE}" \
   --project-directory "${COMPOSE_PROJ_DIR}" \
@@ -42,10 +106,14 @@ docker-compose \
   | tr -d "\r" \
   > "${BACKUP_DIR}/items.json";
 
-# sync Item assets
-rsync -avPh --delete --delete-excluded --exclude .gitignore "${MEDIA_DIR}" "${BACKUP_DIR}";
+log "Syncing Item images from ${MEDIA_DIR} to ${BACKUP_DIR}";
+rsync -avPh ${quiet} --delete --delete-excluded --exclude .gitignore "${MEDIA_DIR}" "${BACKUP_DIR}";
 
-# Commit and push changes
+
 cd "${BACKUP_DIR}";
-git commit -am "Update $(date +%F)";
-git push origin;
+
+log "Commiting changes";
+git commit ${quiet} -am "Update $(date +%F)";
+
+log "Pushing changes";
+git push ${quiet} origin;
